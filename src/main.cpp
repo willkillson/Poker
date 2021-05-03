363b8236af6081172e4692d0b81b2ef3f7a6eaa6
Kevin <willkillson@gmail.com>
2021-05-03 16:01:37 -0700

    basic keyboard relay implemented

--- src/main.cpp
+++ src/main.cpp
@@ -6,78 +6,137 @@
 
 
 
-//Decimal Indexed ASSCII map
-const uint8_t asciiMap[128] =
+#define SHIFT 0x80
+const uint8_t asciimap[128] =
 {
-  //Hex       CHAR       DEC
-	0x00,   //  NUL         0
-	0x01,   //  SOH         1
-	0x02,   //  STX         2
-	0x03,   //  ETX         3
-	0x04,   //  EOT         4
-	0x05,   //  ENQ         5
-	0x06,   //  ACK         6
-	0x07,   //  BEL         7
-	0x08,		//  BS	        8
-	0x09,		//  TAB         9
-	0x0A,		//  LF          10
-	0x0B,   //  VT          11 
-	0x0C,   //  FF          12
-	0x0D,   //  CR          13
-	0x0E,   //  SO          14
-	0x0F,   //  SI          15
-
-	0x10,   //  DEL         16
-	0x11,   //  DC1         17
-	0x12,   //  DC2         18
-	0x13,   //  DC3         19
-	0x14,   //  DC4         20
-	0x15,   //  NAK         21
-	0x16,   //  SYN         22
-	0x17,   //  ETB         23
-	0x18,   //  CAN         24
-	0x19,   //  EM          25
-	0x1A,   //  SUB         26
-	0x1B,   //  ESC         27
-	0x1C,   //  FS          28
-	0x1D,   //  GS          29
-	0x1E,   //  RS          30
-	0x1F,   //  US          31
-
-	0x20,		//  ' '         32
-	0x21,	  //  !           33
-	0x22,	  //  "           34
-	0x23,   //  #           35
-	0x24,   //  $           36
-	0x25,   //  %           37
-	0x26,   //  &           38
-	0x27,   //  '           39
-	0x28,   //  (           40
-	0x29,   //  )           41
-	0x2A,   //  *           42
-	0x2B,   //  +           43
-	0x2C,   //  ,           44
-	0x2D,   //  -           45
-	0x2E,   //  .           46
-	0x2F,   //  /           47
-
-  0x30,		//  0           48
-	0x31,	  //  1           49
-	0x32,	  //  2           50
-	0x33,   //  3           51
-	0x34,   //  4           52
-	0x35,   //  5           53
-	0x36,   //  6           54
-	0x37,   //  7           55
-	0x38,   //  8           56
-	0x39,   //  9           57
-	0x3A,   //  :           58
-	0x3B,   //  ;           59
-	0x3C,   //  <           60
-	0x3D,   //  =           61
-	0x3E,   //  >           62
-	0x3F,   //  ?           63
-  
+	0x00,             // NUL
+	0x00,             // SOH
+	0x00,             // STX
+	0x00,             // ETX
+	0x00,             // EOT
+	0x00,             // ENQ
+	0x00,             // ACK  
+	0x00,             // BEL
+	0x2a,			// BS	Backspace
+	0x2b,			// TAB	Tab
+	0x28,			// LF	Enter
+	0x00,             // VT 
+	0x00,             // FF 
+	0x00,             // CR 
+	0x00,             // SO 
+	0x00,             // SI 
+	0x00,             // DEL
+	0x00,             // DC1
+	0x00,             // DC2
+	0x00,             // DC3
+	0x00,             // DC4
+	0x00,             // NAK
+	0x00,             // SYN
+	0x00,             // ETB
+	0x00,             // CAN
+	0x00,             // EM 
+	0x00,             // SUB
+	0x00,             // ESC
+	0x00,             // FS 
+	0x00,             // GS 
+	0x00,             // RS 
+	0x00,             // US 
+	0x2c,		   //  ' '
+	0x1e|SHIFT,	   // !
+	0x34|SHIFT,	   // "
+	0x20|SHIFT,    // #
+	0x21|SHIFT,    // $
+	0x22|SHIFT,    // %
+	0x24|SHIFT,    // &
+	0x34,          // '
+	0x26|SHIFT,    // (
+	0x27|SHIFT,    // )
+	0x25|SHIFT,    // *
+	0x2e|SHIFT,    // +
+	0x36,          // ,
+	0x2d,          // -
+	0x37,          // .
+	0x38,          // /
+	0x27,          // 0
+	0x1e,          // 1
+	0x1f,          // 2
+	0x20,          // 3
+	0x21,          // 4
+	0x22,          // 5
+	0x23,          // 6
+	0x24,          // 7
+	0x25,          // 8
+	0x26,          // 9
+	0x33|SHIFT,      // :
+	0x33,          // ;
+	0x36|SHIFT,      // <
+	0x2e,          // =
+	0x37|SHIFT,      // >
+	0x38|SHIFT,      // ?
+	0x1f|SHIFT,      // @
+	0x04|SHIFT,      // A
+	0x05|SHIFT,      // B
+	0x06|SHIFT,      // C
+	0x07|SHIFT,      // D
+	0x08|SHIFT,      // E
+	0x09|SHIFT,      // F
+	0x0a|SHIFT,      // G
+	0x0b|SHIFT,      // H
+	0x0c|SHIFT,      // I
+	0x0d|SHIFT,      // J
+	0x0e|SHIFT,      // K
+	0x0f|SHIFT,      // L
+	0x10|SHIFT,      // M
+	0x11|SHIFT,      // N
+	0x12|SHIFT,      // O
+	0x13|SHIFT,      // P
+	0x14|SHIFT,      // Q
+	0x15|SHIFT,      // R
+	0x16|SHIFT,      // S
+	0x17|SHIFT,      // T
+	0x18|SHIFT,      // U
+	0x19|SHIFT,      // V
+	0x1a|SHIFT,      // W
+	0x1b|SHIFT,      // X
+	0x1c|SHIFT,      // Y
+	0x1d|SHIFT,      // Z
+	0x2f,          // [
+	0x31,          // bslash
+	0x30,          // ]
+	0x23|SHIFT,    // ^
+	0x2d|SHIFT,    // _
+	0x35,          // `
+	0x04,          // a
+	0x05,          // b
+	0x06,          // c
+	0x07,          // d
+	0x08,          // e
+	0x09,          // f
+	0x0a,          // g
+	0x0b,          // h
+	0x0c,          // i
+	0x0d,          // j
+	0x0e,          // k
+	0x0f,          // l
+	0x10,          // m
+	0x11,          // n
+	0x12,          // o
+	0x13,          // p
+	0x14,          // q
+	0x15,          // r
+	0x16,          // s
+	0x17,          // t
+	0x18,          // u
+	0x19,          // v
+	0x1a,          // w
+	0x1b,          // x
+	0x1c,          // y
+	0x1d,          // z
+	0x2f|SHIFT,    // {
+	0x31|SHIFT,    // |
+	0x30|SHIFT,    // }
+	0x35|SHIFT,    // ~
+	0				// DEL
 };
 
 int buttonPin = 9;  // Set a button to any pin
@@ -134,7 +193,7 @@ void loop() {
     strcpy(buff,inputString.c_str());
     const char delim[2] = " ";
 
-    Serial.println(inputString);
+    //Serial.println(inputString);
     
     str = strtok(buff,delim);  
     f_bit = strtoul((const char*)str, NULL,16);
@@ -145,14 +204,14 @@ void loop() {
     str = strtok(NULL,delim);
     t_bit = strtoul((const char*)str, NULL,16);
 
-    Serial.print("f_bit: ");
-    Serial.print(f_bit);
+    // Serial.print("f_bit: ");
+    // Serial.print(f_bit);
     // Serial.print(" ");
     // Serial.print("s_bit: ");
     // Serial.print(s_bit);
-    Serial.print(" ");
-    Serial.print("t_bit: ");
-    Serial.println(t_bit);
+    // Serial.print(" ");
+    // Serial.print("t_bit: ");
+    // Serial.println(t_bit);
     
 
     /*
@@ -165,27 +224,25 @@ void loop() {
       //key down
       //s_bit
 
-      switch(t_bit){
-        case 0x1e:
-          Keyboard.write('1');
-          break;
-        case 0x1f:
-          Keyboard.write('2');
-          break;
+
+      for(uint8_t i = 0;i< 128;i++){
+        if(asciimap[i]==t_bit){
+          Keyboard.press(i);
+        }
+      }
+      
+
+
+
+    }else{
+
+      for(uint8_t i = 0;i< 128;i++){
+        if(asciimap[i]==t_bit){
+          Keyboard.release(i);
+        }
       }
 
-      //Keyboard.press(Keyboard.convertMegaToLeonardKeyMap(s_bit));
-    }else{
-      // //key up
-      //   switch(t_bit){
-      //   case 0x1e:
-      //     Keyboard.release('1');
-      //     break;
-      //   case 0x1f:
-      //     Keyboard.release('2');
-      //     break;
-      // }
-      //Keyboard.release(Keyboard.convertMegaToLeonardKeyMap(s_bit));
+
     }
 
 
